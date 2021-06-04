c7bf72aaeaa41e3066cb0fe0c8ac13be431168c4
chandranshu <chandranshu.awasthi@bt.com>
2021-06-04 21:49:30 +0530

    pushing the code

---
+++ atomic-reference-example/src/IllegalAccessException.java
@@ -0,0 +1,24 @@
+public class IllegalAccessException extends   RuntimeException {
+
+
+
+    public IllegalAccessException() {
+    }
+
+    public IllegalAccessException(String message) {
+        super(message);
+    }
+
+    public IllegalAccessException(String message, Throwable cause) {
+
+        super(message, cause);
+    }
+
+    public IllegalAccessException(Throwable cause) {
+        super(cause);
+    }
+
+    public IllegalAccessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
